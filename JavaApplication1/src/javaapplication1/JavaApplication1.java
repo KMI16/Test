a89ce0cffcba2d8abcb4e42f4f05630f73e80244
KMI16 <yourgitemail@web.de>
2018-11-04 22:34:23 +0000

    Added test jframe

---
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author root
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Hello World");
+        frame.setSize(500,500);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        // TODO add some components
+        
+        frame.setVisible(true);
+    }
+    
+}
